34fcd363579d4ab7212e09ea3d17095bf2a30c90
Emmanuel Chêne <emma.chene@gmail.com>
2018-03-06 16:17:38 +0100

    Ex 1 à 4

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -5,59 +5,78 @@ import java8.data.Data;
 import java8.data.Person;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
-
 
 /**
  * Exercice 02 - Map
  */
 public class Lambda_02_Test {
 
-    // tag::PersonToAccountMapper[]
-    interface PersonToAccountMapper {
-        Account map(Person p);
-    }
-    // end::PersonToAccountMapper[]
+	// tag::PersonToAccountMapper[]
+	interface PersonToMapper<E, S> {
+		S map(E e);
+	}
+	// end::PersonToAccountMapper[]
 
-    // tag::map[]
-    private List<Account> map(List<Person> personList, PersonToAccountMapper mapper) {
-        // TODO implémenter la méthode pour transformer une liste de personnes en liste de comptes
-        return null;
-    }
-    // end::map[]
+	// tag::map[]
+	private <E, S> List<S> map(List<E> personList, PersonToMapper<E, S> mapper) {
+		// TODO implémenter la méthode pour transformer une liste de personnes
+		// en liste de comptes
 
+		List<S> result = new ArrayList<>();
 
-    // tag::test_map_person_to_account[]
-    @Test
-    public void test_map_person_to_account() throws Exception {
+		for (E p : personList) {
 
-        List<Person> personList = Data.buildPersonList(100);
+			S unCompte = mapper.map(p);
+			result.add(unCompte);
 
-        // TODO transformer la liste de personnes en liste de comptes
-        // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
+		}
 
-        assert result.size() == personList.size();
-        for (Account account : result) {
-            assert account.getBalance().equals(100);
-            assert account.getOwner() != null;
-        }
-    }
-    // end::test_map_person_to_account[]
+		return result;
+	}
+	// end::map[]
 
-    // tag::test_map_person_to_firstname[]
-    @Test
-    public void test_map_person_to_firstname() throws Exception {
+	// tag::test_map_person_to_account[]
+	@Test
+	public void test_map_person_to_account() throws Exception {
 
-        List<Person> personList = Data.buildPersonList(100);
+		List<Person> personList = Data.buildPersonList(100);
 
-        // TODO transformer la liste de personnes en liste de prénoms
-        List<String> result = null;
+		// TODO transformer la liste de personnes en liste de comptes
+		// TODO tous les objets comptes ont un solde à 100 par défaut
+		List<Account> result = map(personList,  p -> {
+				
+				Account account = new Account();
+				
+				account.setBalance(100);
+				account.setOwner(p);
+				
+				return account;	
+			
+		});
 
-        assert result.size() == personList.size();
-        for (String firstname : result) {
-            assert firstname.startsWith("first");
-        }
-    }
-    // end::test_map_person_to_firstname[]
+		assert result.size() == personList.size();
+		for (Account account : result) {
+			assert account.getBalance().equals(100);
+			assert account.getOwner() != null;
+		}
+	}
+	// end::test_map_person_to_account[]
+
+	// tag::test_map_person_to_firstname[]
+	@Test
+	public void test_map_person_to_firstname() throws Exception {
+
+		List<Person> personList = Data.buildPersonList(100);
+
+		// TODO transformer la liste de personnes en liste de prénoms
+		List<String> result = map(personList, p -> p.getFirstname());
+		
+		assert result.size() == personList.size();
+		for (String firstname : result) {
+			assert firstname.startsWith("first");
+		}
+	}
+	// end::test_map_person_to_firstname[]
 }
